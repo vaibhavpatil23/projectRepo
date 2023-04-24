93a907ad6882a80a8784003dd74e44c2238f8354
vaibhav <vpatil7382@gmail.com>
2023-04-24 12:51:07 +0530

    prototype

--- OOP.js/prototype.js
+++ OOP.js/prototype.js
@@ -1,1 +1,14 @@
-console.log("vaibahv")
+console.log("vaibahv");
+let a = {
+  name: "vaibhav",
+  passion: "stocktrading",
+};
+console.log(a)
+
+let p = {
+  run: () => {
+    alert("program run");
+  },
+};
+a.__proto__ = p;
+a.run();
