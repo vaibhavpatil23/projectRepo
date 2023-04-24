c0e7fa7b44dc5445d026fe7d50b4f0b0ac4f684e
vaibhav <vpatil7382@gmail.com>
2023-04-24 13:28:35 +0530

    user quation

--- OOP.js/prototype.js
+++ OOP.js/prototype.js
@@ -1,14 +1,39 @@
-console.log("vaibahv");
-let a = {
-  name: "vaibhav",
-  passion: "stocktrading",
-};
-console.log(a)
+// console.log("vaibahv");
+// let a = {
+//   name: "vaibhav",
+//   passion: "stocktrading",
+// };
+// console.log(a)
 
-let p = {
-  run: () => {
-    alert("program run");
-  },
-};
-a.__proto__ = p;
-a.run();
+// let p = {
+//   run: () => {
+//     alert("program run");
+//   },
+// };
+// a.__proto__ = p;
+// a.run();
+
+// (1....User)
+function User(name){
+  this.name = name;
+  
+  console.log(this);
+}
+
+User.prototype.printName = function(){
+console.log(this.name)
+}
+
+
+let vaibhav = new User("vaibhav")
+vaibhav.printName()
+
+
+console.log(User.__proto__)
+
+console.log(vaibhav.__proto__ === User.prototype) 
+
+console.log(User.prototype.isPrototypeOf(vaibhav))
+
+User.prototype.species = "Homo Sapiens"
+
