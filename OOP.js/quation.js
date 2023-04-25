fd3313c16c9b744910858187dafd20020cfdbc90
vaibhav <vpatil7382@gmail.com>
2023-04-25 11:40:44 +0530

    quation in oop

--- OOP.js/quation.js
+++ OOP.js/quation.js
@@ -1,31 +1,25 @@
-class Cars { 
+class Cars {
+  constructor(name, maker, price) {
+    this.name = name;
 
-    constructor(name, maker, price) { 
+    this.maker = maker;
 
-      this.name = name; 
+    this.price = price;
+  }
 
-      this.maker =  maker; 
+  getDetails() {
+    return `The name of the car is ${this.name}.and car price is ${this.price}`;
+  }
+}
 
-      this.price = price; 
+let car1 = new Cars("Rolls Royce Ghost", "Rolls Royce", "$315K");
 
-    } 
+let car2 = new Cars("Mercedes AMG One", "Mercedes", "$2700K");
 
-    getDetails(){ 
+console.log(car1.name);
+console.log(car1.maker);
+console.log(car1.price);
 
-        return (`The name of the car is ${this.name}.and car price is ${this.price}`) 
+console.log(car2.maker);
 
-    } 
-
-  } 
-
-  let car1 = new Cars('Rolls Royce Ghost', 'Rolls Royce', '$315K'); 
-
-  let car2 = new Cars('Mercedes AMG One', 'Mercedes', '$2700K'); 
-
-  console.log(car1.name);     
-  console.log(car1.maker);     
-  console.log(car1.price);     
-
-//   console.log(car2.maker);   
-
-  console.log(car1.getDetails()); 
+console.log(car1.getDetails());
